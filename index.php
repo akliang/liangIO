6bf73764032dfad6dde80ca2b599bef698b7c28d
albert73 <albert73@ecbiz172.inmotionhosting.com>
2017-08-08 16:40:42 -0400

    version2 of link shortener... only depends on a single links.csv file

--- index.php
+++ index.php
@@ -1,3 +1,25 @@
+<?php
+
+$key=$_GET['u'];
+unset($url);
+unset($error);
+
+if (isset($key)) {
+  $handle = fopen('links.csv','r');
+
+  while (($data = fgetcsv($handle)) !== FALSE) {
+    if ($data[0] == $key) {
+      $url=$data[1];
+      break;
+    }
+  }
+
+  if (isset($url)) {  header("Location: $url");  }
+  else { $error="<div class=errordiv>Invalid code.</div>"; }
+}
+
+?>
+
 <html>
 
 <head>
@@ -56,17 +78,16 @@ body {
 <a href="./index.php"><img src=logo.png class=logo /></a>
 </div>
 
-<form action=go.php method=get>
+<form action=index2.php method=get>
 <div class=codediv>
-<input type=text name=code class=code id=code>
+<input type=text name=u class=code id=code>
 </div>
 </form>
 
 <?php
-if (isset($_GET["e"])) {
-echo "<div class=errordiv>Invalid code.</div>";
+if (isset($error)) {
+  echo "$error";
 }
-
 ?>
 
 </body>
