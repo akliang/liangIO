cdfc3267b89d238e15aa24cbf224d6dc038ddb46
albert73 <albert73@ecbiz172.inmotionhosting.com>
2017-08-08 16:39:46 -0400

    first version of shortcuts page, relied on folder structure with index files

---
+++ index.php
@@ -0,0 +1,73 @@
+<html>
+
+<head>
+<style>
+body {
+  background-image:url('./metal_bg.jpg');
+  background-size:cover;
+  font-family:Tahoma;
+}
+
+.logodiv {
+  width:100%;
+  text-align:center;
+  margin-top:10%;
+}
+
+.logo {
+  border:none;
+}
+
+.codediv {
+  width:100%;
+  text-align:center;
+  margin-top:2%;
+}
+
+.code {
+  -webkit-border-radius: 5px;
+  -moz-border-radius: 5px;
+  border-radius: 5px;
+  height:50px;
+  width:250px;
+  text-align:center;
+  font-size:24px;
+  background-color:#000;
+  border:none;
+  color:#47a319;
+}
+
+.code:hover {
+  border:2px groove #47a319;
+}
+
+.errordiv {
+  width:100%;
+  color:red;
+  text-align:center;
+}
+
+</style>
+</head>
+
+<body onLoad="document.getElementById('code').focus()">
+
+<div class=logodiv>
+<a href="./index.php"><img src=logo.png class=logo /></a>
+</div>
+
+<form action=go.php method=get>
+<div class=codediv>
+<input type=text name=code class=code id=code>
+</div>
+</form>
+
+<?php
+if (isset($_GET["e"])) {
+echo "<div class=errordiv>Invalid code.</div>";
+}
+
+?>
+
+</body>
+</html>
